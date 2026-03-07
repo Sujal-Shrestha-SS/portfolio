e5e584cfec7c6a2a6e44f2066d0857bcba669328
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-07 18:24:33 +0545

    Setting up nav routes

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Home from './components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="/projects" element={<Home />} />
+        <Route path="/design" element={<Home />} />
+        <Route path="/contact" element={<Home />} />
+      </Routes>
+
     
+    
+    </BrowserRouter>
   )
 }
 
