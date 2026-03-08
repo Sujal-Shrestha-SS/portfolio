74c9774f007876455fb329c2d46a5ba0d7df5de1
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-08 19:12:55 +0545

    Removing react router

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import About from './components/About'
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
+    // <BrowserRouter>
+    //   <Routes>
 
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Home />} />
-        <Route path="/projects" element={<Home />} />
-        <Route path="/design" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
-      </Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<Home />} />
+    //     <Route path="/projects" element={<Home />} />
+    //     <Route path="/design" element={<Home />} />
+    //     <Route path="/contact" element={<Home />} />
+    //   </Routes>
 
     
     
-    </BrowserRouter>
+    // </BrowserRouter>
+    <>
+    <Home />
+    
+    </>
   )
 }
 
 export default App
+
