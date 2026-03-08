72f217d0cb6a2329b26359017206ffadb61a2c84
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-08 19:18:03 +0545

    Creating Navbar links component

---
+++ my-portfolio/src/components/NavbarMenu.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavbarMenu = () => {
+  return (
+   <Navbar id="home">
+      <NavLink href="#home">Home</NavLink>
+      <NavLink href="#about">About Me</NavLink>
+      <NavLink href="#projects">Projects</NavLink>
+      <NavLink href="#design">Graphic Design</NavLink>
+      <NavLink href="#contact">Contact</NavLink>
+    </Navbar>
+  )
+}
+
+export default NavbarMenu
+
+const Navbar = styled.nav`
+  display: flex;
+  gap: 120px;
+  justify-content: center;
+  padding: 45px 60px;
+  font-size: 24px;
+  
+
+`
+
+const NavLink = styled.a`
+  text-decoration: none;
+  color: white;
+  position: relative;
+
+  &:hover{
+    color: #00ffa6;
+  }
+
+  &::after{
+    content: "";
+    width: 0%;
+    height: 2px;
+    background: #00ffa6;
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    transition: 0.3s;
+  }
+
+  &:hover::after{
+    width: 100%;
+  }
+ 
+
+`
