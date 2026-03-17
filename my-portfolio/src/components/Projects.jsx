2ed5e563a5a5a96f2a4e7d92b385b3eaafc90404
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-17 12:07:12 +0545

    Defining Projects component

---
+++ my-portfolio/src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Projects = () => {
+  return (
+    <Container>
+      Personal Projects
+    </Container>
+  )
+}
+
+export default Projects
+
+const Container = styled.div`
+  min-height: 100vh;
+`
