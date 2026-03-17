ed19a6477d266fce7434318b1cb72a45c60e44b8
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-17 11:20:39 +0545

    About section components

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -9,6 +9,39 @@ const About = () => {
         <ProfileImage src="images/profile.png" alt="Profile Picture" />
       </ImageCard>
 
+      <RightContent>
+
+        <AboutTitle>About Me</AboutTitle>
+
+        <AboutText>
+          I am an aspiring IT Engineer passionate about web development,
+          UI design and building modern web applications using React.
+        </AboutText>
+
+        <InfoRow>
+
+          <InfoCard>
+            <CardTitle>Education</CardTitle>
+            <CardText>
+              IT Engineering <br/>
+              Pokhara University <br />
+              2023 - Present
+            </CardText>
+          </InfoCard>
+
+          <InfoCard>
+            <CardTitle>Tech Stack</CardTitle>
+            <CardText>
+              HTML CSS JavaScript <br/>
+              React Styled Components <br />
+              Git Figma
+            </CardText>
+          </InfoCard>
+
+
+        </InfoRow>
+      </RightContent>
+
     </Container>
   )
 }
@@ -54,4 +87,56 @@ const ProfileImage = styled.img`
     ${ImageCard}:hover &{
       transform: scale(1.08);
     }
+`
+
+const RightContent = styled.div`
+    flex: 1;
+    color: white;
+`
+
+const AboutTitle = styled.h2`
+    font-size: 48px;
+    margin-bottom: 20px;
+`
+
+const AboutText = styled.p`
+    max-width: 500px;
+    font-size: 18px;
+    line-height: 1.7;
+    color: #cfd8dc;
+    margin-bottom: 40px;
+`
+
+const InfoRow = styled.div`
+  display: flex;
+  gap: 30px;
+`
+
+const InfoCard = styled.div`
+  flex: 1;
+  padding: 25px;
+  border-radius: 20px;
+
+  background: rgba(255,255,255,0.05);
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255,255,255,0.1);
+
+  transition: transform 0.3s ease;
+
+  &:hover{
+    transform: translateY(-8px);
+  }
+
+  
+
+`
+
+const CardTitle = styled.h3`
+  font-size: 22px;
+  margin-bottom: 10px;
+`
+
+const CardText = styled.p`
+  color: #cfd8dc;
+  line-height: 1.6;
 `
