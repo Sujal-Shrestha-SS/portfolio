249cd28a80c15c4e7888c48200f4c8c710ead6c0
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-17 12:22:51 +0545

    Defining Projects component

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -5,6 +5,20 @@ const Projects = () => {
   return (
     <Container>
       Personal Projects
+
+      <ProjectWrapper>
+
+        <ProjectBlock>
+          <ImageCard>
+            
+            <ProjectImage src="images/TaskCtrl.png" alt="Todo App" />
+
+          </ImageCard>
+
+          <ProjectInfo>This is a todo app</ProjectInfo>
+        </ProjectBlock>
+
+      </ProjectWrapper>
     </Container>
   )
 }
@@ -13,4 +27,25 @@ export default Projects
 
 const Container = styled.div`
   min-height: 100vh;
+`
+
+const ProjectWrapper = styled.div`
+  width: 60%;
+`
+
+const ProjectBlock = styled.div`
+
+`
+
+const ImageCard = styled.div`
+
+`
+
+const ProjectImage = styled.img`
+  
+
+`
+
+const ProjectInfo = styled.h2`
+
 `
