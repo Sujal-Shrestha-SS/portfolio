2f04af57571d9c51974b82d06b178ad0c50c015b
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-18 16:28:55 +0545

    defining styled components

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FaEnvelope, FaMapMarkerAlt, FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa'
 
 const Contact = () => {
   return (
     <Container>
+      <ContentWrapper>
+
+     
         <LeftBlock>
-          <GetinTouch>
+          <GetInTouch>
           <Title>Get in Touch</Title>
 
             <InfoRow>
@@ -17,7 +22,7 @@ const Contact = () => {
               <span>Kathmandu, Nepal</span>
             </InfoRow>
 
-          </GetinTouch>
+          </GetInTouch>
 
           <Divider />
 
@@ -25,7 +30,7 @@ const Contact = () => {
           <Title>Socials</Title>
 
           <SocialIcons>
-              <a href="https://linkedin.com/sujalshresthaa" target="_blank">
+              <a href="https://linkedin.com/in/sujalshresthaa" target="_blank">
                 <FaLinkedin />
               </a>
 
@@ -48,8 +53,81 @@ const Contact = () => {
 
 
         </RightBlock>
+         </ContentWrapper>
     </Container>
   )
 }
 
 export default Contact
+
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(135deg, #0b1d3a, #0f3d2e);
+  color: white;
+  display: flex;
+  
+  align-items: center;
+`
+const ContentWrapper = styled.div`
+    width: 85%;
+    display: flex;
+    gap: 80px;
+    align-items: center;
+    margin: auto;
+    
+`
+
+const LeftBlock = styled.div`
+  width: 40%;
+  padding: 40px;
+  background: #0f172a;
+  color: white;
+`
+
+const RightBlock = styled.div`
+  width: 40%;
+  padding: 40px;
+`
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+`
+
+const GetInTouch = styled.div`
+
+`
+
+const InfoRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin: 10px 0;
+
+  svg {
+    font-size: 20px;
+  } //styling the icon
+`
+
+const Divider = styled.hr`
+  margin: 30px 0;
+  border: 0.5px solid #444;
+`
+
+const Socials = styled.div``
+
+const SocialIcons = styled.div`
+  display: flex;
+  gap: 20px;
+
+  a {
+    color: white;
+    font-size: 24px;
+    transition: 0.3s;
+  }
+
+  a:hover {
+    color: #38bdf8;
+    transform: scale(1.2);
+  }
+`
