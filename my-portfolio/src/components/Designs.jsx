35badc75bf60f59b4a9307ca2e48f1d8ca128706
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-18 15:39:54 +0545

    defining styled components

--- my-portfolio/src/components/Designs.jsx
+++ my-portfolio/src/components/Designs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import designs from '../data/designsData'
+import styled from 'styled-components'
 
 const Designs = () => {
   return (
@@ -12,7 +13,7 @@ const Designs = () => {
             <DesignBlock key={index}>
 
               <DesignImage src={design.image} alt={design.title} />
-              
+
             </DesignBlock>
 
         ))}
@@ -23,3 +24,44 @@ const Designs = () => {
 }
 
 export default Designs
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(135deg, #0b1d3a, #0f3d2e);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Title = styled.h1`
+  font-size: 42px;
+  margin-top: 100px;
+  margin-bottom: 50px;
+`
+
+const DesignWrapper = styled.div`
+  width: 60%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); // 2 per row
+  gap: 40px;
+`
+
+const DesignBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`
+
+const DesignImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  transition: transform 0.4s ease;
+
+  ${DesignBlock}:hover &{
+    transform: scale(1.08);
+  }
+
+`
