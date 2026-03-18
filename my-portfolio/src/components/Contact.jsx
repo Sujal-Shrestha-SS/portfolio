9c738894fa75f06084871ed39ecaaae1f975998b
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-03-18 16:03:47 +0545

    Defining contact component

---
+++ my-portfolio/src/components/Contact.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <Container>
+        <LeftBlock>
+          <GetinTouch>
+          <Title>Get in Touch</Title>
+
+
+          <Icon /> Gmail
+          <Icon /> Location
+
+          </GetinTouch>
+
+          <hr></hr>
+
+          <Socials>
+          <Title>Socials</Title>
+
+          <Icon />
+          <Icon />
+          <Icon />
+          </Socials>
+
+        </LeftBlock>
+
+        <RightBlock>
+          <Title>Contact Me</Title>
+
+
+        </RightBlock>
+    </Container>
+  )
+}
+
+export default Contact
